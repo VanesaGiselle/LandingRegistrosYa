1737745de166e7eedc21aad3e415df1c06b1d92d
Vanesa Korbenfeld <vkorbenfeld@gmail.com>
2022-01-19 15:08:32 -0300

    Initial commit

---
+++ envio-contacto.php
@@ -0,0 +1,40 @@
+﻿<?php 
+
+$nombre_us = $_POST ['nombre'];
+$mail_us = $_POST ['mail'];
+$telefono_us = $_POST ['telefono'];
+$mensaje_us = $_POST ['mensaje'];
+$archivo = $_POST ['archivo'];
+
+/*
+echo "$nombre_us <br/>";
+echo "$mail_us <br/>";
+echo "$telefono_us <br/>";
+echo "$mensaje_us <br/>";
+echo "$adjunto_us <br/>";
+*/
+
+
+if(!($_POST['nombre']) || !($_POST['mail']) || ($_POST['mensaje']) ){
+	 
+	 header("location:index.php?r=error");
+	 
+}else{
+
+$destino = 'info@registrosya.com';
+$asunto = 'Contacto desde mi sitio www.ingeniame.com';
+$remitente = 'From: mail_us';
+
+$mensaje = "Nombre:".$nombre_us."\r\n";
+$mensaje .= "Mail:".$mail_us."\r\n";
+$mensaje .= "Teléfono:".$telefono_us."\r\n";
+$mensaje .= "Mensaje:".$mensaje_us."\r\n";
+$mensaje .= "Adjunto:".$archivo;
+
+
+mail($destino, $asunto, $mensaje, $remitente);
+
+	header("location:index.php?r=ok");
+
+ };
+?>
---
+++ index.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>RegistrosYa</title>
+	<!-- FAVICON-->
+	<link rel="shortcut icon" type="image/png" href="imgs/favicon.png"/>
+	
+    <!-- CSS de Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">    
+	<!-- CSS de Autor -->
+	<link href="css/misestilos.css" rel="stylesheet"/>
+	<!-- CSS de Font-Awesome -->
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	
+ 
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
+<header>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<nav class="subheader">
+			<div class="row">
+				<div class="col-md-4"></div>	
+				<div class="col-xs-6 col-md-4 subheader-style">
+					<p class="subheader-menu"><span class="glyphicon glyphicon-phone">  </span>+54 (011) 11 <span class="text-bold">4700-0456</span></p>
+				</div>	
+				<div class="col-xs-6 col-md-4 subheader-style">
+					<p class="subheader-menu"><span class="glyphicon glyphicon-envelope">  </span>info@<span class="text-bold">registrosya</span>.com</p>
+				</div>
+			</div>
+		</nav>
+	  <div class="container-fluid">
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>                        
+		  </button>
+		  <a class="navbar-brand" href="#myPage"><img class="navbar-brand-img" src="imgs/RegistrosYa-Logotipo.png" alt="RegistrosYa"/></a>
+		</div>
+		<div class="collapse navbar-collapse" id="myNavbar">
+		  <ul class="nav navbar-nav navbar-right">
+			<li><a class="navbar-style" href="#myPage">Home</a></li>
+			<li><a class="navbar-style" href="#registros">Registros</a></li>
+			<li><a class="navbar-style" href="#nosotros">Nosotros</a></li>
+			<li><a class="navbar-style contact" href="#contacto">Contacto</a></li>
+		  </ul>
+		</div>
+	  </div>
+	</nav>
+</header><!-- fin header -->
+
+<section id="myPage">
+	<div id="home">	
+		<div class="home-principal img-principal">	
+				<div class="content-h1">
+					<div class="content-orange">
+						<h1>¡HACÉ TU <b>REGISTRO YA</b>!</h1>
+						<p class="subtitle">Contáctenos desde el <span class="text-bold">Interior del País</span> o donde se encuentre</p>
+					</div>
+				</div>
+				<div class="home-text">
+					<p class="home-phase">Registramos sus <span class="text-bold">MARCAS – PATENTES – MODELOS –</span> en Argentina y en el Exterior</p>
+					<p class="home-phase">Hacer valer su derecho de autor también es nuestra tarea</p>
+					<p class="home-phase">Registramos <span class="text-bold">SOFTWARES- APPS - PÁGINAS WEBS - OBRAS MUSICALES- LITERARIAS - PROGRAMAS DE TELEVISIÓN - CUADROS - REVISTAS - OBRAS DE TEATRO</span></p>
+				</div>
+		</div>
+	</div>
+</section><!-- fin myPage -->
+
+<section id="registros">
+
+	<div class="row">
+		<div class="col-xs-12 col-md-4">
+			<div class="img-phase">
+				<img class="image-responsive candado" src="imgs/candado.png" alt="Registro de marca"/>
+			</div>
+		</div>
+		<div class="col-xs-12 col-md-6">
+			<h2 class="title-phase">PARA TENER EN CUENTA</h2>
+			<p class="text-phase">Cuando creamos un producto, brindamos un servicio o desarrollamos cualquier actividad original, inédita, sumamente útil, que posee características novedosas ante los ojos de los demás, nos volvemos atractivos y corremos un riesgo, alguien puede apropiarse de nuestra creación.</p>
+			<p class="text-phase2">Nuestro compromiso es asesorarlo respetando la confidencialidad de su proyecto, y realizar los diversos registros para que usted esté protegido.</p>
+			<p class="text-phase-bold"><span class="text-bold"> El registro le otorga derechos sobre su obra y la oportunidad de comercializar, licenciar, vender o difundir tranquilo.</span></p>
+		</div>
+	</div>
+	<div class="tipos-de-registros">
+		<div class="panel-group" id="accordion">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+				  <h4 class="panel-title">
+				  <a class="panel-title-style" data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+					REGISTRO DE MARCAS</a>
+				  </h4>
+				</div>
+				<div id="collapse1" class="panel-collapse collapse">
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-xs-12 col-md-4 brand">
+								<div class="registro-marca">
+									<img class="image-responsive marca" src="imgs/img-marca.png" alt="Marca"/>
+									<h3 class="preg-marca">¿Qué es una marca?</h3>
+									<p class="text-marca">Es el signo que distingue un producto o servicio de otro. La marca juega un papel indispensable dentro de la competencia comercial.</p>
+								</div>
+							</div>						
+							<div class="col-xs-12 col-md-8 column-preg">
+								<div class="marca-preg-1">
+									<h3 class="title-marca-preg-1">¿Por qué registrar mi marca?</h3>
+									<p class="text-marca-preg-2">Es la única manera de obtener <span class="text-bold">protección legal</span> y evitar que otra persona la utilice, puede otorgar licencias, transferirla o venderla. Le brinda un lugar en el mercado, para que su negocio crezca.</p>
+								</div>							
+								<div class="marca-preg-1">
+									<h3 class="title-marca-preg">¿Cuánto tarda el trámite de Registro?</h3>
+									<p class="text-marca-preg-2">Desde el momento que se inicia la solicitud hasta el momento que se concede el título de marca, tarda aproximadamente <span class="text-bold">18 meses</span>.</p>
+								</div>
+							</div>
+						</div>
+						<div class="row world">
+							<div class="col-xs-12 col-md-2 column-world">
+								<div class="img-world">
+									<img class="image-responsive image-world" src="imgs/img-mundo.png" alt="Mundo"/>
+								</div>
+							</div>						
+							<div class="col-xs-12 col-md-10 column-world2">
+								<div class="text-world">
+									<p class="">Realizamos la búsqueda de antecedentes de <span class="text-bold">Marcas en Argentina GRATIS</span>. </p>
+									<p class="">Les brindamos asesoramiento profesional para contestación de vistas y negociación de Oposiciones.</p>
+									<p class="">Realizamos registros en el país del mundo que usted lo necesite <span class="text-bold">CONSÚLTENOS</span>.</p>
+								</div>							
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="panel panel-default">
+				<div class="panel-heading">
+				  <h4 class="panel-title">
+					<a class="panel-title-style" data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+					REGISTRO DE PATENTES y/o MODELOS DE UTILIDAD</a>
+				  </h4>
+				</div>
+				<div id="collapse2" class="panel-collapse collapse">
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-xs-12 col-md-6">
+								<div class="registro-marca">
+									<h3 class="preg-marca">¿Por qué registrar mi Patente o Modelo de Utilidad?</h3>
+									<p class="text-marca">Le brinda el <span class="text-bold">derecho de exclusividad</span> sobre el invento, permitiéndote accionar sobre terceros que sin su consentimiento realicen ofertas de venta, fabricación, uso o procedimiento patentado.</p>
+								</div>
+							</div>						
+							<div class="col-xs-12 col-md-6">
+								<div class="">
+									<h3 class="title-marca-preg"> ¿Cómo registro mi Patente o Modelo de Utilidad?</h3>
+									<p class="text-marca-preg-2">Si bien no es un procedimiento simple, nuestros profesionales lo asistirán para que su Invención tenga el resguardo adecuado y usted se beneficie, pudiendo explotar su Patente o Modelo de Utilidad, u otorgar licencias para terceros, cobrando las regalías pertinentes.</p>
+								</div>							
+							</div>
+						</div>
+					</div>
+			    </div>
+			</div>
+			<div class="panel panel-default">
+				<div class="panel-heading">
+				  <h4 class="panel-title">
+					<a class="panel-title-style" data-toggle="collapse" data-parent="#accordion" href="#collapse3">
+					REGISTRO DE MODELOS O DISEÑOS INDUSTRIALES</a>
+				  </h4>
+				</div>
+				<div id="collapse3" class="panel-collapse collapse">
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-xs-12 col-md-6 brand">
+								<div class="registro-marca">
+									<img class="image-responsive industrial" src="imgs/img-industrial.png" alt="Diseño Industrial"/>
+									<h3 class="preg-marca">¿Qué es un Modelo o Diseño Industrial? </h3>
+									<p class="text-marca">Comprenden los aspectos estéticos de un producto. Esto le añade valor agregado, haciéndolos más atractivos al consumidor y pudiendo tornarse en el motivo principal de compra del cliente.</p>
+								</div>
+							</div>						
+							<div class="col-xs-12 col-md-6 column-industrial">
+								<div class="marca-preg-1">
+									<h3 class="title-marca-preg-1">¿Por qué registrar mi Modelo o Diseño Industrial? </h3>
+									<p class="text-marca-preg-2">Es la única manera de obtener <span class="text-bold">protección legal</span> y evitar que otra persona la utilice, puede otorgar licencias, transferirla o venderla. Le brinda un lugar en el mercado, para que su negocio crezca.</p>
+								</div>							
+								<div class="">
+									<p class="item">Novedad / Originalidad</p>
+									<p class="item">Aplicación Industrial</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>		  
+			<div class="panel panel-default">
+				<div class="panel-heading">
+				  <h4 class="panel-title">
+					<a class="panel-title-style" data-toggle="collapse" data-parent="#accordion" href="#collapse4">
+					DEPÓSITOS EN DERECHO DE AUTOR</a>
+				  </h4>
+				</div>
+				<div id="collapse4" class="panel-collapse collapse">
+					<div class="panel-body depositos-collapse">
+						<h3 class="depositos-type">(Sofwares - Aplicaciones - Páginas Webs)</h3>
+						<div class="">
+							<div class="row">
+								<div class="col-xs-12 col-md-4">
+									<div class="depositos">
+										<img class="image-responsive img-depositos" src="imgs/icon-musica.png" alt="Depósitos de Obras Musicales"/>
+										<h2 class="title-deposito">Depósitos de Obras Musicales</h2>
+									</div>
+								</div>
+								<div class="col-xs-12 col-md-4">
+									<div class="depositos">
+										<img class="image-responsive img-depositos" src="imgs/icon-cuadro.png" alt="Cuadros"/>
+										<h2 class="title-deposito">Cuadros</h2>
+									</div>
+								</div>
+								<div class="col-xs-12 col-md-4">
+									<div class="depositos">
+										<img class="image-responsive img-depositos" src="imgs/icon-television.png" alt="Programas de Televisión"/>
+										<h2 class="title-deposito">Programas de Televisión</h2>
+									</div>
+								</div>
+							</div>
+							
+							<div class="row">
+								<div class="col-xs-12 col-md-4">
+									<div class="depositos">
+										<img class="image-responsive img-depositos" src="imgs/icon-obrasdeteatro.png" alt="Obras de teatro"/>
+										<h2 class="title-deposito">Obras de teatro</h2>
+									</div>
+								</div>
+								<div class="col-xs-12 col-md-4">
+									<div class="depositos">
+										<img class="image-responsive img-depositos" src="imgs/icon-revistas.png" alt="Revistas"/>
+										<h2 class="title-deposito">Revistas</h2>
+									</div>
+								</div>
+								<div class="col-xs-12 col-md-4">
+									<div class="depositos">
+										<img class="image-responsive img-depositos" src="imgs/icon-libros.png" alt="Depósitos de Obras Literarias"/>
+										<h2 class="title-deposito">Depósitos de Obras Literarias</h2>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div><!-- fin accordion -->
+	<div class="contactenos">
+		<h2>Contáctenos y le enviaremos la cotización del trámite que necesite</h2>
+	</div>
+</section><!-- fin registros -->
+
+<section id="nosotros">
+	<div class="nosotros-registrosya">
+		<img class="img-nosotros" src="imgs/RegistrosYa-Logotipo.png" type="image/png" alt="RegistrosYa"/>
+		<p class="nosotros-text">Desde el año 2005, nuestros clientes nos confían sus registros, porque les brindamos celeridad, un ámbito confidencial, cordial y de tranquilidad para generar sus proyectos.
+		<span class="text-bold">RegistrosYa</span> es un consultor online que asesora y realiza diversos registros y depósitos, referidos a la <span class="text-italic">Propiedad Industrial e Intelectual</span>, avocados a la protección de los intereses y derechos de las empresas, comercios y personas.
+		Registrar es proteger lo propio, es la manera de demostrar autoría o derecho y defenderse de terceros que pretendan plagiar su obra, sea cual fuere.
+		</p>
+		<p class="nosotros-text-phase"><span class="text-bold">¡Cada uno de nuestros clientes, es importante para nosotros!</span></p>
+	</div>
+</section><!-- fin nosotros -->
+
+<section id="contacto">
+	<div class="container-contact">
+		<h2 class="contacto-title">CONTACTO</h2>
+	</div>
+<div class="contact-form">	
+	<div class="row">
+		<div class="col-xs-12 col-md-4">
+			<div class="info-contact">
+				<p class="contacto-text">Consulte si es posible registrar su invención, ingrese cuál es el trámite que necesita y lo asesoraremos a la brevedad <span class="text-bold">gratuitamente</span>.</p>
+				<p class="contacto-info"><span class="glyphicon glyphicon-phone contacto-icon">  </span>+54 (011) 11 <span class="text-bold">4700-0456</span></p>
+				<p class="contacto-info"><span class="glyphicon glyphicon-envelope contacto-icon">  </span>info@<span class="text-bold">registrosya</span>.com</p>
+				<p class="contacto-info"><span class="glyphicon glyphicon-map-marker contacto-icon">  </span>Nuestras oficinas están en <span class="text-bold">Zona Norte</span></p>
+			</div>
+		</div>
+		<div class="col-xs-12 col-md-8">
+			<form method="POST" action="envio-contacto.php" class="formulario">
+			  <fieldset class="form-group">
+				<label for="nombre"></label>
+				<input type="name" class="form-control" id="nombre" placeholder="Nombre">
+			  </fieldset>
+			  <fieldset class="form-group">
+				<label for="telefono"></label>
+				<input type="telefono" class="form-control" id="telefono" placeholder="Teléfono / Celular">
+			  </fieldset>
+			  <fieldset class="form-group">
+				<label for="mail"></label>
+				<input type="email" class="form-control" id="mail" placeholder="E-mail">
+			  </fieldset>
+			  <fieldset class="form-group">
+				<label for="mensaje"></label>
+				<textarea class="form-control"  placeholder="Mensaje" id="mensaje" rows="5"></textarea>
+			  </fieldset> 
+
+			  <input type="file" class="select-archivo" name="archivo"/><br />	  
+			  <input type="submit" class="btn-enviar"></input>
+			</form>
+		</div>
+	</div>
+</div>
+</section>
+
+
+<footer>
+	<div><span class="text-bold">RegistrosYa</span> © 2016</div>
+</footer><!-- fin footer -->
+
+
+    <!-- Librería jQuery requerida por los plugins de JavaScript -->
+    <script src="http://code.jquery.com/jquery.js"></script> 
+    <!-- Todos los plugins JavaScript de Bootstrap (también puedes
+         incluir archivos JavaScript individuales de los únicos
+         plugins que utilices) -->
+    <script src="js/bootstrap.min.js"></script>  
+	<!-- desde jQuery activamos plugins y costumizamos comportamientos -->    
+	<script>
+	$('.toggle-sidebar').click(function(){
+	 $('body').toggleClass('sidebar-open');
+	 return false;
+	});
+	$(document).ready(function(){
+	  // Inicializar Tooltip de bootstrap
+	  $('[data-toggle="tooltip"]').tooltip();  
+	  
+	  // suavizar el scroll a los links en navbar + footer 
+	  $(".navbar a, footer a[href='#myPage']").on('click', function(event) { 
+		// verificar el valor de this.hash antes de sobreescribir el comportamiento por defecto
+		if (this.hash !== "") { 
+		  // prevenir que se dispare el evento click 
+		  event.preventDefault(); 
+		  // guardar hash
+		  var hash = this.hash; 
+		  // con el método animate() de la librería de jQuery suavizamos el scroll
+		  // el valor opcional (900) es el número en milisegundos que le toma en llegar al area especificada 
+		  $('html, body').animate({
+			scrollTop: $(hash).offset().top
+		  }, 900, function(){ 
+			// Agregar el valor hash (#) en la URL cuando termine el scroll
+			window.location.hash = hash;
+		  });
+		} 
+	  });
+	})
+	</script>
+
+	
+	
+	
+</body>
